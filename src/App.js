001e4d624edc497276cdf4cd5f7b975be10c5123
1010017k <uekawauematu@gmail.com>
2023-06-30 16:23:09 +1200

    third

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Col, Row } from 'react-bootstrap';
+import Login from './Login';
 
 function App() {
   return (
-    <h1>Hello!
-    </h1>
+    <div className="Container app-container" role="parent">
+      <Row>
+        <Col>
+           <h1>Test</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Login data-testid="child" />
+        </Col>
+      </Row>
+    </div>
+    
   );
 }
 
